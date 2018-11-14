f59a959da7cc29d52bdc440dae766732486655d0
arbaz khan <arbaz.k@bewakoof.com>
2018-11-14 13:32:38 +0530

    code done for google login need to check later

--- login_details.js
+++ login_details.js
@@ -2,5 +2,9 @@ module.exports={
     facebook_1:{
         user_name:"carsyn.toluwanimi@lcelandic.com",    
         password:"bewa123$"
+    },
+    google_1:{
+        user_name:"bewakoofandroid@gmail.com",
+        password:"bewa123$"
     }
 }
--- pages/login.js
+++ pages/login.js
@@ -5,7 +5,12 @@ module.exports = {
     login_submit: "//button[@id='desktop_submit']",
     profile_icon: "//i[@id='profile_icon']",
     profile_logout: "//a[@id='logout']",
-    facebook_button:"//body/div[@id='app']/div/div/div/div[@class='backdrop']/div[contains(@class,'loginPopup')]/div/div/div[@class='_Auth_bodyHolder']/div[@class='_RenderSocialLogin_socialLogins']/div[@class='_RenderSocialLogin_connectWithWrapper']/button[1]",
-    facebook_username:"//input[@id='email']",
-    facebook_password:"//input[@id='pass']"
+
+    facebook_button: "//body/div[@id='app']/div/div/div/div[@class='backdrop']/div[contains(@class,'loginPopup')]/div/div/div[@class='_Auth_bodyHolder']/div[@class='_RenderSocialLogin_socialLogins']/div[@class='_RenderSocialLogin_connectWithWrapper']/button[1]",
+    facebook_username: "//input[@id='email']",
+    facebook_password: "//input[@id='pass']",
+
+    google_button: "//body/div[@id='app']/div/div/div/div[@class='backdrop']/div[contains(@class,'loginPopup')]/div/div/div[@class='_Auth_bodyHolder']/div[@class='_RenderSocialLogin_socialLogins']/div[@class='_RenderSocialLogin_connectWithWrapper']/button[2]",
+    google_username: "//*[@id='identifierId']",
+    google_password: "//input[@name='password']"
 }
